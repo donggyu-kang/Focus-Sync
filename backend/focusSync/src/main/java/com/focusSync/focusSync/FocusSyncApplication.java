fa489823bd355a960992897839528c286b897e8a
kangdongdyu <kangkg0067@gmail.com>
2026-02-22 18:06:08 +0900

    feat(be): 백엔드 스프링 부트 환경 설정 및 보안 파일 제외 (#1)

---
+++ backend/focusSync/src/main/java/com/focusSync/focusSync/FocusSyncApplication.java
@@ -0,0 +1,13 @@
+package com.focusSync.focusSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusSyncApplication.class, args);
+	}
+
+}
---
+++ backend/focusSync/src/test/java/com/focusSync/focusSync/FocusSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focusSync.focusSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
